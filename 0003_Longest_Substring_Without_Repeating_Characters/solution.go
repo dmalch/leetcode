e632e0a0cb16ae47c0d1a49b0076b02820eb0eff
Dmitry Malchikov <dmalch@gmail.com>
2023-02-06 23:46:59 +0100

    03 Longest Substring Without Repeating Characters
    Optimized solution

--- 0003_Longest_Substring_Without_Repeating_Characters/solution.go
+++ 0003_Longest_Substring_Without_Repeating_Characters/solution.go
@@ -1,25 +1,23 @@
 package main
 
+import "math"
+
 func lengthOfLongestSubstring(s string) int {
 	pos1 := 0
 	pos2 := 0
 
 	maxLength := 0
 
-	chars := make(map[byte]int)
+	chars := make([]int, math.MaxUint8)
 	for pos2 < len(s) {
 		pos2++
 
 		charAt := s[pos2-1]
 
-		if _, ok := chars[charAt]; ok {
-			chars[charAt]++
-			for chars[charAt] > 1 {
-				chars[s[pos1]]--
-				pos1++
-			}
-		} else {
-			chars[charAt] = 1
+		chars[charAt]++
+		for chars[charAt] > 1 {
+			chars[s[pos1]]--
+			pos1++
 		}
 
 		length := pos2 - pos1
