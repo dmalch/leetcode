83765b7c803d7ba1e9c3d2b3bac5eabd7ddec356
Dmitry Malchikov <dmalch@gmail.com>
2023-02-14 00:40:08 +0100

    08 String to Integer (atoi)
    Solution

--- 0008_String_to_Integer/solution.go
+++ 0008_String_to_Integer/solution.go
@@ -1,5 +1,54 @@
 package main
 
+import "math"
+
 func myAtoi(s string) int {
-	return 0
+	if s == "" {
+		return 0
+	}
+
+	result := 0
+
+	i := 0
+
+	// skip leading white spaces
+	for ; i < len(s); i++ {
+		if s[i] != ' ' {
+			break
+		}
+	}
+
+	if i >= len(s) {
+		return 0
+	}
+
+	sign := 1
+	if s[i] == '+' {
+		i++
+	} else if s[i] == '-' {
+		sign = -1
+		i++
+	}
+
+	for ; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			break
+		}
+
+		num := int(s[i] - '0')
+		num *= sign
+
+		result *= 10
+
+		if sign > 0 && math.MaxInt32-result < num {
+			result = math.MaxInt32
+			break
+		} else if math.MinInt32-result > num {
+			result = math.MinInt32
+			break
+		}
+		result += num
+	}
+
+	return result
 }
--- 0008_String_to_Integer/solution_test.go
+++ 0008_String_to_Integer/solution_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -8,14 +9,54 @@ import (
 
 func Test(t *testing.T) {
 	tests := []struct {
-		name string
-		s    string
+		name     string
+		s        string
+		expected int
 	}{
 		{
-			name: "Example 1",
+			name:     "Example 0",
+			s:        "",
+			expected: 0,
 		},
 		{
-			name: "Example 2",
+			name:     "Example 0.1",
+			s:        " ",
+			expected: 0,
+		},
+		{
+			name:     "Example 1",
+			s:        "42",
+			expected: 42,
+		},
+		{
+			name:     "Example 1.1",
+			s:        "123",
+			expected: 123,
+		},
+		{
+			name:     "Example 2",
+			s:        "   -42",
+			expected: -42,
+		},
+		{
+			name:     "Example 2.1",
+			s:        "+-12",
+			expected: 0,
+		},
+		{
+			name:     "Example 3",
+			s:        "4193 with words",
+			expected: 4193,
+		},
+		{
+			name:     "Example 4",
+			s:        "21474836460",
+			expected: math.MaxInt32,
+		},
+		{
+			name:     "Example 5",
+			s:        "-2147483649",
+			expected: math.MinInt32,
 		},
 	}
 	for _, test := range tests {
@@ -25,7 +66,7 @@ func Test(t *testing.T) {
 			s := myAtoi(test.s)
 
 			g.Expect(s).
-				Should(Equal(0))
+				Should(Equal(test.expected))
 		})
 	}
 }
